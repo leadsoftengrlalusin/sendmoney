c1f5743ed33f3e8ce95f0e5b8ad4e327d02819c1
leadsoftengrlalusin <johnrovic.lalusin@gcash.com>
2025-07-25 11:24:57 +0800

    git init

---
+++ src/main/java/com/gxchange/sendmoney/SendmoneyApplication.java
@@ -0,0 +1,13 @@
+package com.gxchange.sendmoney;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SendmoneyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SendmoneyApplication.class, args);
+	}
+
+}
